26a9bedbc72bcee8c24619c85d6c0295a61d86a2
izen <izen@hermod.org>
2018-02-23 11:10:17 -0500

    automatically move vimrc

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp ./.vimrc ~/
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
@@ -9,3 +10,4 @@ git clone https://github.com/ccraciun/vim-dreammaker.git ~/.vim/bundle/vim-dream
 git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
 git clone git://github.com/nathanaelkane/vim-indent-guides.git ~/.vim/bundle/vim-indent-guides
 git clone https://github.com/tomlion/vim-solidity.git ~/.vim/bundle/vim-solidity
+cd
